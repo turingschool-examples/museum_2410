dbd5bc826f6adc88361b36f2893db1bee9426342
Kevin Newland <kevin.newland@yahoo.com>
2024-10-30 12:54:05 -0500

    write tests for museum class

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -78,12 +78,64 @@ RSpec.describe Museum do
       
       
       dmns.add_exhibit(imax)
-      
+
       patron_2.add_interest("IMAX")
 
       dmns.recommend_exhibits(patron_2)
 
       expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+    end
+  end
+
+  describe '#patrons_by_exhibit_interest' do
+    it 'returns a Hash where each key is an Exhibit' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+
+      patron_1 = Patron.new("Bob", 0)
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+
+      patron_2 = Patron.new("Sally", 20)
+      patron_2.add_interest("Dead Sea Scrolls")
+
+      patron_3 = Patron.new("Johnny", 5)
+      patron_3.add_interest("Dead Sea Scrolls")
+
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+
+      expect(dmns.patrons_by_exhibit_interest).to be_a(Hash)
+    end
+  end
+
+  describe 'ticket_lottery_contestants' do
+    xit ' returns an array of patrons that do not have enough money to see an exhibit, but are interested in that exhibit' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      dmns.add_exhibit(dead_sea_scrolls)
+
+      patron_1 = Patron.new("Bob", 0)
+      patron_1.add_interest("Dead Sea Scrolls")
+
+      patron_2 = Patron.new("Sally", 20)
+      patron_2.add_interest("Dead Sea Scrolls")
+
+      patron_3 = Patron.new("Johnny", 5)
+      patron_3.add_interest("Dead Sea Scrolls")
+
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+
+      expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to be_a(Hash)
     end
   end
 end
