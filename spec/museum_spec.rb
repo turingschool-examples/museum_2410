26fcf7b2ffa49ab35062d3ecd14e92b9b5a39bef
Dustin Peukert <dpeukert04@gmail.com>
2024-10-30 11:42:49 -0400

    write test for Museum class #recommend_exhibits

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -55,4 +55,25 @@ RSpec.describe Museum do
       expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
     end
   end
+
+  describe '#recommend_exhibits' do
+    it 'recommends all exhibits to a patron that matches their interests' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+      patron_1 = Patron.new("Bob", 20)
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_1.add_interest("Gems and Minerals")
+      patron_2 = Patron.new("Sally", 20)
+      patron_2.add_interest("IMAX")
+
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+
+      expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
+      expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+    end
+  end
 end
