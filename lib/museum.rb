97a4e07247934c23227276bc5f4d60b346d1aab7
elysableu <elysabward@gmail.com>
2024-10-30 10:28:16 -0600

    Feat: add recommend_exhibits behavior

--- lib/museum.rb
+++ lib/museum.rb
@@ -9,4 +9,14 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    recommended = []
+    @exhibits.each do |exhibit|
+      if patron.interests.include?(exhibit.name)
+        recommended << exhibit
+      end
+    end
+    return recommended
+  end
 end
