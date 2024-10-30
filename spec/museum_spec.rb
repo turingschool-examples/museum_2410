fb59676f619e0d3ffe9ed22e2a2d5befa7ad6e5c
Dustin Peukert <dpeukert04@gmail.com>
2024-10-30 12:31:41 -0400

    write test for Museum class #ticket_lottery_contestants

--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -38,7 +38,7 @@ RSpec.describe Museum do
       dmns.add_exhibit(gems_and_minerals)
       dmns.add_exhibit(dead_sea_scrolls)
       dmns.add_exhibit(imax)
-      expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+      expect(dmns.exhibits).to contain_exactly(gems_and_minerals, dead_sea_scrolls, imax)
     end
 
     it 'does not affect other museums' do
@@ -54,7 +54,7 @@ RSpec.describe Museum do
       dmns.add_exhibit(dead_sea_scrolls)
       dmns.add_exhibit(imax)
       expect(vlm.exhibits).to eq([])
-      expect(dmns.exhibits).to eq([gems_and_minerals, dead_sea_scrolls, imax])
+      expect(dmns.exhibits).to contain_exactly(gems_and_minerals, dead_sea_scrolls, imax)
     end
   end
 
@@ -74,8 +74,8 @@ RSpec.describe Museum do
       dmns.add_exhibit(dead_sea_scrolls)
       dmns.add_exhibit(imax)
 
-      expect(dmns.recommend_exhibits(patron_1)).to eq([dead_sea_scrolls, gems_and_minerals])
-      expect(dmns.recommend_exhibits(patron_2)).to eq([imax])
+      expect(dmns.recommend_exhibits(patron_1)).to contain_exactly(dead_sea_scrolls, gems_and_minerals)
+      expect(dmns.recommend_exhibits(patron_2)).to contain_exactly(imax)
     end
   end
 
@@ -116,9 +116,36 @@ RSpec.describe Museum do
       patron_3.add_interest("Dead Sea Scrolls")
 
       expect(dmns.patrons_by_exhibit_interest.keys.count).to eq(3)
-      expect(dmns.patrons_by_exhibit_interest).to include(dead_sea_scrolls => [patron_1, patron_2, patron_3])
-      expect(dmns.patrons_by_exhibit_interest).to include(gems_and_minerals => [patron_1])
-      expect(dmns.patrons_by_exhibit_interest).to include(imax => [])
+      expect(dmns.patrons_by_exhibit_interest[dead_sea_scrolls]).to contain_exactly(patron_1, patron_2, patron_3)
+      expect(dmns.patrons_by_exhibit_interest[gems_and_minerals]).to contain_exactly(patron_1)
+      expect(dmns.patrons_by_exhibit_interest[imax]).to contain_exactly()
+    end
+  end
+
+  describe '#ticket_lottery_contestants' do
+    it 'returns an array of patrons too poor to attend an exhibit of interest' do
+      dmns = Museum.new("Denver Museum of Nature and Science")
+      gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+      dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+      imax = Exhibit.new({name: "IMAX",cost: 15})
+      patron_1 = Patron.new("Bob", 0)
+      patron_2 = Patron.new("Sally", 20)
+      patron_3 = Patron.new("Johnny", 5)
+
+      patron_1.add_interest("Gems and Minerals")
+      patron_1.add_interest("Dead Sea Scrolls")
+      patron_2.add_interest("Dead Sea Scrolls")
+      patron_3.add_interest("Dead Sea Scrolls")
+
+      dmns.add_exhibit(gems_and_minerals)
+      dmns.add_exhibit(dead_sea_scrolls)
+      dmns.add_exhibit(imax)
+
+      dmns.admit(patron_1)
+      dmns.admit(patron_2)
+      dmns.admit(patron_3)
+
+      expect(dmns.ticket_lottery_contestants(dead_sea_scrolls)).to contain_exactly(patron_1, patron_3)
     end
   end
 end
