c82938aecbb8ae51151e0e3eefd42a5c8b912f6e
Dustin Peukert <dpeukert04@gmail.com>
2024-10-30 11:50:20 -0400

    write Museum class method #recommend_exhibits

--- lib/museum.rb
+++ lib/museum.rb
@@ -10,4 +10,14 @@ class Museum
   def add_exhibit(exhibit)
     @exhibits << exhibit
   end
+
+  def recommend_exhibits(patron)
+    recommended_exhibits = @exhibits.find_all do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
+    
+    recommended_exhibits.sort_by! do |exhibit|
+      exhibit.name
+    end
+  end
 end
