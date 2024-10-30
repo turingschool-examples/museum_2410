2bc2138509d6cffbc894c33e7ba23005853d31be
Jillian Bleggi <jillian.bleggi@gmail.com>
2024-10-30 11:47:41 -0400

    Update Museum lib/spec files to complete Iteration 2

--- lib/museum.rb
+++ lib/museum.rb
@@ -5,4 +5,12 @@ class Museum
     @name = name
     @exhibits = []
   end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    @exhibits.select { |exhibit| patron.interests.include?(exhibit.name) }
+  end
 end
--- spec/museum_spec.rb
+++ spec/museum_spec.rb
@@ -17,4 +17,40 @@ RSpec.describe Museum do
       expect(dmns.exhibits).to eq []
     end
   end
+
+  it 'can add exhibits' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    expect(dmns.exhibits.count).to eq 3
+  end
+
+  it 'can recommend interests to patrons' do
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    imax = Exhibit.new({name: "IMAX",cost: 15})
+
+    dmns.add_exhibit(gems_and_minerals)
+    dmns.add_exhibit(dead_sea_scrolls)
+    dmns.add_exhibit(imax)
+
+    patron_1 = Patron.new("Bob", 20)
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    patron_2 = Patron.new("Sally", 20)
+    patron_2.add_interest("IMAX")
+
+    expect(dmns.recommend_exhibits(patron_1).count).to eq 2
+    expect(dmns.recommend_exhibits(patron_2).count).to eq 1
+  end
 end
