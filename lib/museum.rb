60f204b610da4e43b6815c75a2e6dd6a31e8da31
Dustin Peukert <dpeukert04@gmail.com>
2024-10-30 13:16:05 -0400

    write Museum class #ticket_lottery_contestants method

--- lib/museum.rb
+++ lib/museum.rb
@@ -40,4 +40,10 @@ class Museum
 
     patron_interests
   end
+
+  def ticket_lottery_contestants(exhibit)
+    contestants = patrons_by_exhibit_interest[exhibit].find_all do |patron|
+      patron.spending_money < exhibit.cost
+    end
+  end
 end
